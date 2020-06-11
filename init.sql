2aba23a302237e79111161b4a5df38f46bbcb5c0
ReeceBirchmore <reecebirchmore@gmail.com>
2020-06-11 17:01:56 +0100

    Fixing

--- init.sql
+++ init.sql
@@ -1,10 +1,13 @@
-CREATE TABLE IF NOT EXISTS quizzes (
+
+
+
+CREATE TABLE quizzes (
     id SERIAL PRIMARY KEY,
     title  text,
     allowback BOOLEAN,
 );
 
-CREATE TABLE IF NOT EXISTS questions (
+CREATE TABLE questions (
     id SERIAL PRIMARY KEY,
     question  TEXT,
     quizid TEXT,
@@ -12,29 +15,26 @@ CREATE TABLE IF NOT EXISTS questions (
     options TEXT [],
 );
 
-CREATE TABLE IF NOT EXISTS answers (
+CREATE TABLE answers (
     id SERIAL PRIMARY KEY,
     answers  TEXT,
     quizid TEXT
 );
 
 
-INSERT INTO Quizzes (title, quizid, allowback) VALUES('No Back Button', '26JBU', false);
-INSERT INTO Quizzes (title, quizid, allowback) VALUES('Allow Back Button', '27JBU', true);
+INSERT INTO quizzes (title, quizid, allowback) VALUES('No Back Button', '26JBU', false);
+INSERT INTO quizzes (title, quizid, allowback) VALUES('Allow Back Button', '27JBU', true);
 
+INSERT INTO questions (question, quizid, input) VALUES('Is lybin fit?', '26JBU', 'text');
+INSERT INTO questions (question, quizid, input) VALUES('Is Lybin honestly not the fittest character?', '26JBU', 'text');
+INSERT INTO questions (question, quizid, input) VALUES('What would you rate Lybin out of 5?', '26JBU', 'text');
+INSERT INTO questions (question, quizid, input) VALUES('How do you personally feel Lybin has shaped your world?', '26JBU', 'text');
+INSERT INTO questions (question, quizid, options, input) VALUES('What do you love the most about our lord Lybin?', '26JBU', '{That damned smile, his amazing body, his voice}', 'multi-select');
+INSERT INTO questions (question, quizid, input) VALUES('How do you personally feel Lybin has shaped your world?', '26JBU', 'text');
 
-
-INSERT INTO Questions (question, quizid, input) VALUES('Is lybin fit?', '26JBU', 'text');
-INSERT INTO Questions (question, quizid, input) VALUES('Is Lybin honestly not the fittest character?', '26JBU', 'text');
-INSERT INTO Questions (question, quizid, input) VALUES('What would you rate Lybin out of 5?', '26JBU', 'text');
-INSERT INTO Questions (question, quizid, input) VALUES('How do you personally feel Lybin has shaped your world?', '26JBU', 'text');
-INSERT INTO Questions (question, quizid, options, input) VALUES('What do you love the most about our lord Lybin?', '26JBU', '{That damned smile, his amazing body, his voice}', 'multi-select');
-INSERT INTO Questions (question, quizid, input) VALUES('How do you personally feel Lybin has shaped your world?', '26JBU', 'text');
-
-
-INSERT INTO Questions (question, quizid, input) VALUES('Is lybin fit?', '27JBU', 'text');
-INSERT INTO Questions (question, quizid, input) VALUES('Is Lybin honestly not the fittest character?', '27JBU', 'text');
-INSERT INTO Questions (question, quizid, input) VALUES('What would you rate Lybin out of 5?', '27JBU', 'text');
-INSERT INTO Questions (question, quizid, input) VALUES('How do you personally feel Lybin has shaped your world?', '27JBU', 'text');
-INSERT INTO Questions (question, quizid, options, input) VALUES('What do you love the most about our lord Lybin?', '27JBU', '{That damned smile, his amazing body, his voice}', 'multi-select');
-INSERT INTO Questions (question, quizid, input) VALUES('How do you personally feel Lybin has shaped your world?', '27JBU', 'text');
+INSERT INTO questions (question, quizid, input) VALUES('Is lybin fit?', '27JBU', 'text');
+INSERT INTO questions (question, quizid, input) VALUES('Is Lybin honestly not the fittest character?', '27JBU', 'text');
+INSERT INTO questions (question, quizid, input) VALUES('What would you rate Lybin out of 5?', '27JBU', 'text');
+INSERT INTO questions (question, quizid, input) VALUES('How do you personally feel Lybin has shaped your world?', '27JBU', 'text');
+INSERT INTO questions (question, quizid, options, input) VALUES('What do you love the most about our lord Lybin?', '27JBU', '{That damned smile, his amazing body, his voice}', 'multi-select');
+INSERT INTO questions (question, quizid, input) VALUES('How do you personally feel Lybin has shaped your world?', '27JBU', 'text');
